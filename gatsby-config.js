ca0359c50efabd185294d408b35dca614cb1534e
jwsyee <102253733+jwsyee@users.noreply.github.com>
2022-03-23 14:56:50 -0400

    Update gatsby-config to make admin page work

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-netlify-identity`,
       options: {
-        url: `https://thescienceseed-demo.ml` // required!
+        url: `https://kaleidoscopic-tartufo-4c9e93.netlify` // required!
       }
     },
     {
